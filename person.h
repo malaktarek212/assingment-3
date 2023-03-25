1a808dac204e3510ca4d124c53adf7def31defa6
malaktarek212 <124691056+malaktarek212@users.noreply.github.com>
2023-03-26 01:45:04 +0200

    Update person.h

--- person.h
+++ person.h
@@ -1,1 +1,24 @@
+#ifndef PERSON_H
+#define PERSON_H
 
+class Person {
+protected:
+  string name;
+  int id;
+  int age;
+
+public:
+  Person() {}
+  Person(string name1, int id1, int age1) {
+  }
+  string get_name() const {  }
+  void set_name(string name1) {  }
+  int get_id() const {  }
+  void set_id(int id1) {  }
+
+  int get_age() const { }
+  void set_age(int age1) {  }
+
+  void printinfo() const {
+  }
+};
